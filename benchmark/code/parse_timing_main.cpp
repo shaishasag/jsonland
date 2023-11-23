ca85776b4ae5bf4ee79d3e62d4520e87ac848c97
ShaiShasag <shaishasag@yahoo.co.uk>
2023-11-23 17:28:18 +0200

    examples files moved to benchmark folder

--- benchmark/code/parse_timing_main.cpp
+++ benchmark/code/parse_timing_main.cpp
@@ -7,9 +7,21 @@
 
 jsonland::json_node create_array()
 {
+    jsonland::json_node a_num_from_num(17);
+    jsonland::json_node a_num_from_string("300.123", jsonland::number_t);
+
+    jsonland::json_node jStr1("mamaluka", jsonland::string_t);
+    
     // create an object
     jsonland::json_node jObj1(jsonland::object_t);
-    jObj1["one"] = 1;
+    jsonland::json_node a_false(false);
+    jsonland::json_node a_true(true);
+    jsonland::json_node a_string("stringify");
+    jObj1["a_num_from_num"] = a_num_from_num;
+    jObj1["a_num_from_string"] = a_num_from_string;
+    jObj1["a_false"] = a_false;
+    jObj1["a_true"] = a_true;
+    jObj1["a_string"] = a_string;
     jObj1["two"] = "two";
  
     // create another object
@@ -85,7 +97,7 @@ struct sep_1000 : std::numpunct<char>
 };
  
 
-void parse_and_report_file(std::filesystem::path& in_file)
+void parse_and_report_file(std::filesystem::path& in_file, bool in_situ=true)
 {
     std::cout.imbue(std::locale(std::cout.getloc(), new sep_1000));
     std::cout << "parsing " << in_file << std::endl;
@@ -95,14 +107,23 @@ void parse_and_report_file(std::filesystem::path& in_file)
     
     auto before = std::chrono::steady_clock::now();
     jsonland::json_doc jdoc;
-    int err = jdoc.parse_insitu(contents);
+    int err = 0;
+    std::string parse_type;
+    if (in_situ) {
+        parse_type = "insitue";
+        err = jdoc.parse_insitu(contents);
+    }
+    else {
+        parse_type = "copy";
+        err = jdoc.parse(contents);
+    }
     auto after = std::chrono::steady_clock::now();
     if (0 == err)
     {
         //j_example_1.dump(std::cout);
         auto duration = std::chrono::duration<double, std::milli>(after - before).count();
         uint64_t byte_per_mili = double(contents.size()) / double(duration);
-        std::cout << "    " << "parsing of " << contents.size() << " bytes " << std::fixed << duration << "ms;";
+        std::cout << "    " << "parsing " << "(" << parse_type << ") of " << contents.size() << " bytes " << std::fixed << duration << "ms;";
         std::cout << " " << byte_per_mili << " bytes per millisecond" << std::endl;
         size_t memory_consumption = jdoc.memory_consumption();
         uint64_t mem_ratio = double(memory_consumption) / double(contents.size());
@@ -114,34 +135,48 @@ void parse_and_report_file(std::filesystem::path& in_file)
     }
 }
 
-int main(int argc, const char * argv[])
+static void report_size_of()
 {
-//    auto jArr = create_array();
-//    jArr.dump(std::cout);
-//    std::cout << std::endl;
-//    jArr[0]["two"].dump(std::cout);
-//    std::cout << std::endl;
-
-    std::filesystem::path example_folder = std::filesystem::path(__FILE__);
-    std::vector<std::filesystem::path> path_vec;
-    path_vec.push_back(std::filesystem::path(__FILE__).parent_path().append("example_1.json"));
-    path_vec.push_back(std::filesystem::path(__FILE__).parent_path().append("example_2.json"));
-    path_vec.push_back(std::filesystem::path(__FILE__).parent_path().append("example_3.json"));
-
-    for (auto& path : path_vec)
-    {
-        parse_and_report_file(path);
-    }
-    
     std::cout << std::endl;
     std::cout << "sizeof(json_node): " << sizeof(jsonland::json_node) << std::endl;
     std::cout << "    sizeof(json_node.m_node_type): " << sizeof(jsonland::json_node::m_node_type) << std::endl;
-    std::cout << "    sizeof(json_node.m_str_v): " << sizeof(jsonland::json_node::m_value) << std::endl;
-
+    std::cout << "    sizeof(json_node.m_value): " << sizeof(jsonland::json_node::m_value) << std::endl;
+    
     std::cout << "    sizeof(json_node.m_num): " << sizeof(jsonland::json_node::m_num) << std::endl;
     std::cout << "    sizeof(json_node.m_values): " << sizeof(jsonland::json_node::m_values) << std::endl;
     std::cout << "    sizeof(json_node.m_obj_key_to_index): " << sizeof(jsonland::json_node::m_obj_key_to_index) << std::endl;
     std::cout << "    sizeof(json_node.m_key): " << sizeof(jsonland::json_node::m_key) << std::endl;
+}
+
+int parse_and_report_files()
+{
+    std::filesystem::path file_in_folder = std::filesystem::path(__FILE__); std::cout << file_in_folder << std::endl;
+    //file_in_folder.remove_filename(); std::cout << file_in_folder << std::endl;
+    file_in_folder = file_in_folder.parent_path(); std::cout << file_in_folder << std::endl;
+    file_in_folder = file_in_folder.parent_path(); std::cout << file_in_folder << std::endl;
+    file_in_folder.append("files"); std::cout << file_in_folder << std::endl;
+    file_in_folder.append("example_1.json"); std::cout << file_in_folder << std::endl;
+    std::vector<std::filesystem::path> path_vec;
+    path_vec.push_back(file_in_folder);
+    path_vec.push_back(file_in_folder.replace_filename("example_2.json"));
+    path_vec.push_back(file_in_folder.replace_filename("example_3.json"));
+
+    for (auto& path : path_vec)
+    {
+        parse_and_report_file(path, true);
+    }
+    
 
     return 0;
+}
+
+int main(int argc, const char * argv[])
+{
+    //parse_and_report_files();
+    report_size_of();
+    auto jArr = create_array();
+    jArr.dump(std::cout);
+    std::cout << std::endl;
+    jArr[0]["two"].dump(std::cout);
+    std::cout << std::endl;
 }
